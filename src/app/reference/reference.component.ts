d4dccec8776d55a367b4f66e1ebbee41142411d3
HubbardHarvey3 <hubbard.harvey@gmail.com>
2021-01-31 16:06:58 -0500

    loop through arrays on reference page now

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -7,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferenceComponent implements OnInit {
 
+  bibles: Array<string> = ["King James Version", "New King James Version", "New American Standard Version"]
+  studyBibles: Array<string> = ["Scofield Study System (Original edition, 'Old Scofield', KJV only)", "Ryrie Study Bible, NAS", "Wiersbe Study Bible, NKJV"]
+  studyHelp: Array<string> = ["Cruden's Concordance", "Strong's Exhaustive Concordance"]
+  miscellaneous: Array<{ title: string, author: string }> = [
+    { title: "Sin, The Savior, and Salvation: The Theology of Everlasting Life", author: "Robert P. Lightner" },
+    { title: "Rightly Dividing the Word of Truth", author: "C. I. Scofield" },
+    { title: "Dispensationalism", author: "Lewis Sperry Chafer" },
+    { title: "Dispensationalism", author: "Charles C. Ryrie" },
+    { title: "Basic Theology", author: "Charles C. Ryrie" },
+    { title: "The Moody Handbook of Theology", author: "Paul Enns" },
+    { title: "Revelation", author: "Charles C. Ryrie" },
+    { title: "So Great Salvation", author: "Charles C. Ryrie" },
+    { title: "Biblical Demonology", author: "Merrill F. Unger" }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
